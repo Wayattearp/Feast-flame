1bc33ed1ab099b99f8e8d06099a3ad17eedfd8be
Wayattearp <iason148348@gmail.com>
2023-07-13 13:07:43 -0400

    Add router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 import Nav from './components/Nav';
+import Homepage from './components/Homepage';
+import Booking from './components/Booking';
 
 function App() {
   return (
     <div className='wrapper'>
-      <Header />
-      <Nav />
-      <Main />
-      <Footer />
+      <Router>
+        <Header />
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/booking" element={<Booking />} />
+        </Routes>
+        <Footer />
+      </Router>
+
     </div>
   );
 }
