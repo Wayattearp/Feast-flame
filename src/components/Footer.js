25b4b1ab1fd670553d8cd6c7739b1570c6d5b382
Wayattearp <iason148348@gmail.com>
2023-07-27 13:29:10 -0400

    Add logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer () {
     return (
         <footer>
-            footer
+            <img src="logo.png" alt="Logo" width="200" height="200"/>
         </footer>
     )
 }
