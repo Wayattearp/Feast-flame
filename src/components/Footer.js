4b164aa5b77a4842c2c9bd14aa9c35bd51febb2f
Wayattearp <iason148348@gmail.com>
2023-07-29 19:22:00 -0400

    Add links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,67 @@
 import React from 'react';
 import logo from '../components/logo.png'
+import {
+    faFacebook,
+    faTwitter,
+    faInstagram,
+    faYoutube
+} from '@fortawesome/free-brands-svg-icons';
+import {
+    faEnvelope,
+    faLocationDot,
+    faPhone
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Footer.css';
 
-function Footer () {
+const contacts = [
+    { icon: faLocationDot, info: '123 First ave, Fort Lee, NJ, 07024', },
+    { icon: faPhone, info: '(123) 456-7890', },
+    { icon: faEnvelope, info: 'customer@feast-flame.com', },
+];
+
+const socials = [
+    { icon: faFacebook, name: 'facebook', },
+    { icon: faTwitter, name: 'twitter', },
+    { icon: faInstagram, name: 'instagram', },
+    { icon: faYoutube, name: 'youtube', },
+];
+
+const Footer = () => {
     return (
-        <footer>
-            <img src={logo} alt="Logo" width="120" height="120"/>
+        <footer className="site-footer">
+            <div className="container grid">
+                <img
+                    className="site-footer-logo"
+                    src={logo}
+                    alt="Little Lemon"
+                />
+                <div className="site-footer-contact">
+                    <h4>Contact us</h4>
+                    <address>
+                        {contacts.map((contact, index) =>
+                            <p key={index}>
+                                <FontAwesomeIcon icon={contact.icon} /> {contact.info}
+                            </p>
+                        )}
+                    </address>
+                </div>
+                <div className="site-footer-social">
+                    <h4>Connect with us</h4>
+                    {socials.map((social, index) =>
+                        <a
+                            key={index}
+                            href={`https://www.${social.name}.com`}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <FontAwesomeIcon icon={social.icon} size="lg" />
+                        </a>
+                    )}
+                </div>
+            </div>
         </footer>
-    )
-}
-export default Footer
+    );
+};
+
+export default Footer;
