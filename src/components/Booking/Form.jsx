5b94d345edd5e56ebe681cebd6ad7f6904a3c96e
Wayattearp <iason148348@gmail.com>
2023-07-29 08:20:54 -0400

    Add form validations

--- src/components/Booking/Form.jsx
+++ src/components/Booking/Form.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 function Form() {
     const [date, setDate] = useState('');
     const [time, setTime] = useState('');
-    const [guests, setGuests] = useState(1);
+    const [guests, setGuests] = useState(2);
     const [occasion, setOccasion] = useState('');
     const [availableTimes] = useState(['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']);
     const navigate = useNavigate();
@@ -15,20 +15,36 @@ function Form() {
         setDate(currentDate);
     }, []); // Run this effect only once on component mount
 
+    const isFormValid = () => {
+        // Implement additional validation rules here
+        if (guests < 1 || guests > 10) {
+            return false;
+        }
+
+        return true;
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault(); // Prevent default form submission behavior
 
-        navigate('/confirmed');
+        if (isFormValid()) {
+            navigate('/confirmed');
+        } else {
+            alert('Please fill in all required fields with valid data.');
+        }
     };
 
+
     return (
-        <form className="reservation-form"onSubmit={handleSubmit}>
+        <form className="reservation-form" onSubmit={handleSubmit}>
             <label htmlFor="res-date">Date:</label>
             <input
                 type="date"
                 id="res-date"
                 value={date}
                 onChange={(e) => setDate(e.target.value)}
+                required // This attribute makes the field required
+                min={new Date().toISOString().split('T')[0]} // Minimum date is today
             />
 
             <label htmlFor="res-time">Time:</label>
@@ -36,6 +52,7 @@ function Form() {
                 id="res-time"
                 value={time}
                 onChange={(e) => setTime(e.target.value)}
+                required // This attribute makes the field required
             >
                 {/* Populate options from the availableTimes state */}
                 {availableTimes.map((time) => (
@@ -46,12 +63,13 @@ function Form() {
             <label htmlFor="guests">Number of guests:</label>
             <input
                 type="number"
-                placeholder="1"
+                placeholder="2"
                 min="1"
                 max="10"
                 id="guests"
                 value={guests}
                 onChange={(e) => setGuests(parseInt(e.target.value))}
+                required // This attribute makes the field required
             />
 
             <label htmlFor="occasion">Occasion:</label>
@@ -59,14 +77,20 @@ function Form() {
                 id="occasion"
                 value={occasion}
                 onChange={(e) => setOccasion(e.target.value)}
+                required // This attribute makes the field required
             >
-                <option>Birthday</option>
-                <option>Anniversary</option>
+                <option value="">Select an occasion</option> {/* Add an empty option to force selection */}
+                <option value="Birthday">Birthday</option>
+                <option value="Anniversary">Anniversary</option>
             </select>
 
-            <input className="reserve-button" type="submit" value="Make Your reservation" />
+            <input
+                className="reserve-button"
+                type="submit"
+                value="Make Your reservation"
+                disabled={!isFormValid()} // Disable the button if the form is not valid
+            />
         </form>
-
     );
 }
 
