b3c330ab24fe36222b35bf8f504d13f5bee25a37
Wayattearp <iason148348@gmail.com>
2023-08-03 00:25:15 -0400

    Design nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 function Nav() {
     return (
         <nav>
-            <a href="https://www.linkedin.com/" target="_blank">FIND US</a>
             <Link to="/booking">RESERVATIONS</Link>
             <Link to="/under-construction">MENU</Link>
-            <Link to="/under-construction">ORDER ONLINE</Link>
             <Link to="/feast-flame">ABOUT</Link>
+            <a href="https://www.linkedin.com/" target="_blank">FIND US</a>
+            <Link to="/under-construction">ORDER ONLINE</Link>
         </nav>
     )
 }
